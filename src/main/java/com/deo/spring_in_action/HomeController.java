831ea029e3bb2135dbe405d7fa6a3a1856f019a0
Serhii Lukianenko <lukyanenko88@gmail.com>
2023-01-27 20:00:47 +0200

    Initial commit

---
+++ src/main/java/com/deo/spring_in_action/HomeController.java
@@ -0,0 +1,15 @@
+package com.deo.spring_in_action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome(){
+        return "home";
+    }
+
+
+}
