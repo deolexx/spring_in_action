e88a37197ea63057460b59685f593a9a3b022ee4
s.lukianenko <lukianenko88@gmail.com>
2023-01-29 19:31:45 +0200

    add /POST support to DesignTacoController added IngredientByIdConverter

---
+++ src/main/java/com/deo/spring_in_action/converter/IngredientByIdConverter.java
@@ -0,0 +1,38 @@
+package com.deo.spring_in_action.converter;
+
+import com.deo.spring_in_action.entity.Ingredient;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Totrilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettyce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+        );
+        ingredients.forEach(ingredient -> ingredientMap.put(ingredient.getName(), ingredient));
+
+    }
+
+    @Override
+    public Ingredient convert(String source) {
+        return ingredientMap.get(source);
+    }
+}
